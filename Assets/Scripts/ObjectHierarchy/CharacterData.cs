95689c47d63d93587fe0120f748f115428026119
Garrett Money <garrettmoney@icloud.com>
2020-08-14 22:45:03 -0500

    Detail Changes
    Player inherits from character. Small change to JSON files

--- Assets/Scripts/ObjectHierarchy/CharacterData.cs
+++ Assets/Scripts/ObjectHierarchy/CharacterData.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using SimpleJSON;
-using System.IO;
+
 
 public class CharacterData : MonoBehaviour
 {
@@ -42,16 +41,5 @@ public class CharacterData : MonoBehaviour
         
         //race = new RaceData();
         //characterClass = new ClassData();
-    }
-    public CharacterData(string characterClass, string race, int level)
-    {
-        string fighterString = File.ReadAllText("Assets/ClassesDatabase/FIGHTER.json");
-        var FighterJSON = JSON.Parse(fighterString);
-        int hitDie = FighterJSON["Fighter"]["hitDie"].AsInt;
-        Debug.Log(hitDie);
-
-        this.level = level;
-        this.race = new RaceData(race);
-        this.characterClass = new ClassData(characterClass); 
     }
 }
--- Assets/Scripts/ObjectHierarchy/PlayerData.cs
+++ Assets/Scripts/ObjectHierarchy/PlayerData.cs
@@ -1,8 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SimpleJSON;
+using System.IO;
 
 public class PlayerData : CharacterData
 {
     public List<Feat> feats;
+
+    public PlayerData(string characterClass, string race, int level)
+    {
+        string fighterString = File.ReadAllText("Assets/ClassesDatabase/FIGHTER.json");
+        var FighterJSON = JSON.Parse(fighterString);
+        int hitDie = FighterJSON["Fighter"]["hitDie"].AsInt;
+        Debug.Log(hitDie);
+
+        this.level = level;
+        this.race = new RaceData(race);
+        this.characterClass = new ClassData(characterClass);
+    }
 }
